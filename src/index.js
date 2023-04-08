aca8c5868e1523ecb02a6d1dc1771a3903af33ac
mr-a360 <wrish502@gmail.com>
2023-04-08 08:36:57 +0700

    edit and fix bug

--- src/index.js
+++ src/index.js
@@ -12,52 +12,44 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-
 require('dotenv').config();
-// set env 
+// set env
 const PORT = process.env.PORT || 3000;
 const TOKEN = process.env.DISCORD_TOKEN;
 
-
 // create a client instance
-const client = new Client(
-    {
-        intents: [
-            GatewayIntentBits.GuildMembers,
-            GatewayIntentBits.GuildMessages,
-            GatewayIntentBits.Guilds,
-            GatewayIntentBits.GuildWebhooks,
-            GatewayIntentBits.MessageContent
-        ]
-    }
-);
+const client = new Client({
+	intents: [
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildWebhooks,
+		GatewayIntentBits.MessageContent,
+	],
+});
 module.exports = client;
-
 
 // create collection
 client.Commands = new Collection();
 client.Components = new Collection();
 
-
 // listen for incoming connections
 app.listen(PORT, () => {
-    logger.log('Server listening on port ' + PORT);
-    // bot login
-    client.login(TOKEN);
+	logger.log('Server listening on port ' + PORT);
+	// bot login
+	client.login(TOKEN);
 });
 client.on('ready', () => {
-    logger.ready('bot is ready');
-    app.locals.botname = client.user.username;
+	logger.ready(`${client.user.username}bot is ready`);
+	app.locals.botname = client.user.username;
 });
-
 
 // render home page
 app.get('/', (req, res) => {
-    res.render('index', {
-        botName: req.app.locals.botname
-    });
+	res.render('index', {
+		botName: req.app.locals.botname,
+	});
 });
-
 
 // connect handler
 require('./handler')(client);
